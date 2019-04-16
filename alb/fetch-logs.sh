e0e18a70ed6a2880378f35fbf9ed9adf63acbde0
Kai Hendry <kai.hendry@unee-t.com>
2019-04-16 15:47:09 +0800

    Ignore -1 values

--- alb/fetch-logs.sh
+++ alb/fetch-logs.sh
@@ -61,7 +61,11 @@ seetoday() {
 	esac
 
 	aws s3 --profile $AWS_PROFILE sync s3://$STAGE-logs-unee-t/$1/AWSLogs/$(acc $STAGE)/elasticloadbalancing/ap-southeast-1/$(date +'%Y/%m/%d/') $STAGE-$1
-	zcat $STAGE-$1/*.log.gz | awk '{print $7, $4, $10, $13, $14, $15}' | sort
+
+	# 1. If the load balancer can't dispatch the request to a target. This can happen if the target closes the connection before the idle timeout or if the client sends a malformed request.
+	# 2. This value can also be set to -1 if the registered target does not respond before the idle timeout.
+
+	zcat $STAGE-$1/*.log.gz | awk '{print $7, $4, $10, $13, $14, $15}' | grep -v ^-1 | sort
 }
 
 seetoday ${1-bugzilla}
